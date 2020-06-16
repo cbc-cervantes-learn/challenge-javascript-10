7cca9afed2a1e76adf63c3cd507ee1375a0901b6
Cristian Bello Cervantes <cristiancervantes.cbc@gmail.com>
2020-06-15 22:39:41 -0500

    Add challenge solution

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,15 @@
-const getMaxNumber = (numbersArray) => {
-  // your code here
-}
+const getMaxNumber = (numbersArray) => numbersArray.reduce((accumulator, currentValue) => accumulator < currentValue  ? currentValue : accumulator);
 
-const getMinNumber = (numbersArray) => {
-  // your code here
-}
+const getMinNumber = (numbersArray) => numbersArray.reduce((accumulator, currentValue) => accumulator > currentValue  ? currentValue : accumulator);
 
 const getAverage = (numbersArray) => {
-  // your code here
+    return numbersArray.reduce((accumulator, currentValue, idx) => {
+        if (idx === numbersArray.length - 1 ){
+            accumulator += currentValue;
+            return accumulator / numbersArray.length;
+        }else{
+            return accumulator + currentValue;
+        }
+    })
 }
-
 module.exports = { getMaxNumber, getMinNumber, getAverage };
